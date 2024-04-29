24a077dde397f80e6e0a6d5e1631c45bdbf1e6d6
Aston Jones <Aston.Jones@ellucian.com>
2024-04-28 20:28:27 -0500

    Added notion database addition and testing out frontend connection with local.

--- index.js
+++ index.js
@@ -2,24 +2,88 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const port = 3000
+const bodyParser = require('body-parser')
+const { Client, APIErrorCode } = require("@notionhq/client")
 
-const contactSecret = process.env.CONTACT_SECRET
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/health', (req, res) => {
+  res.send({level: "info", message: "healthy"})
 })
 
-app.post('/contact', (req, res) => {
-    if(req.body.secret != 'notMuchOfASecret'){
-
+/*
+This endpoint is to put a lead from website into notion and notify a persons 
+Properties we are looking for in the html form
+    property_address: string,
+    name: string,
+    phone_number: number/string,
+    appSecret: string */
+app.post('/contact', async (req, res) => {
+    const databaseId = process.env.NOTION_WEBSITE_LEAD_DB_ID;
+    if(req.body.appSecret == process.env.SERVER_SECRET){
+        try {
+            // get phone from user data
+            let parsedPhone
+            if(req.body.phone_number != null){
+                parsedPhone = Number(req.body.phone_number)
+            } else {
+                parsedPhone = 0
+            }
+            const notion = new Client({ auth: process.env.NOTION_SECRET })
+    
+            const response = await notion.pages.create({
+                parent: {
+                    database_id: databaseId,
+                },
+                properties: {
+                    "Phone": {
+                        "type": "number",
+                        "number": parsedPhone
+                    },
+                    "Address": {
+                        "type": "rich_text",
+                        "rich_text": [
+                            {
+                                "type": "text",
+                                "text": {
+                                    "content": req.body.property_address,
+                                    "link": null
+                                }
+                            }
+                        ]
+                    },
+                    "Name": {
+                        "id": "title",
+                        "type": "title",
+                        "title": [
+                            {
+                                "type": "text",
+                                "text": {
+                                    "content": req.body.name,
+                                },
+                            }
+                        ]
+                    },
+                }
+            });
+            console.log({level:'info', message:'An entry has been added to the database.'});
+            res.sendStatus(200);
+    
+        } catch (error) {
+            if (error.code === APIErrorCode.ObjectNotFound) {
+                console.log({level: 'error', message: 'Error retrieving API key. Check the API key for notion and try again.'});
+            } else {
+                console.error({level: 'error', message:'500 error. Something went wrong.'});
+            }
+        }
+    } else {
+        console.log({level: 'info', message: 'app conflict - not the correct secrete was used to try to access the server.'})
+        res.send(200);
     }
-    res.send('post worked?')
-    // here we want to collect the data
-
-    // send data to notion
-
-    // send data to email or sms -> sms preferred
-})
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
