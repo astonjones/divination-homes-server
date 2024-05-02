3672b6a008d91dec75f0668236367db37d65d807
Aston Jones <Aston.Jones@ellucian.com>
2024-05-02 17:39:59 -0500

    enable cors requests.

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ const express = require('express')
 const app = express()
 const port = 3000
 const bodyParser = require('body-parser')
+const cors = require('cors');
 const { Client, APIErrorCode } = require("@notionhq/client")
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
+app.use(cors())
 
 app.get('/health', (req, res) => {
   res.send({level: "info", message: "healthy"})
